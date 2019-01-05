4c044201ee922400d9059672550a73ee906fa0eb
League <league@Mac-mini.local>
2019-01-05 11:27:35 -0800

    Simon Says - unfinished, Jeopardy - unfinished
    Day 2

--- src/Jeopardy.java
+++ src/Jeopardy.java
@@ -44,17 +44,18 @@ public class Jeopardy implements ActionListener {
 		frame.setLayout(new BorderLayout());
 
 		// 1. Make the frame show up
-
+		frame.setVisible(true);
 		// 2. Give your frame a title
-
+		frame.setTitle("Jeopardy");
 		// 3. Create a JPanel variable to hold the header using the createHeader method
-
+		JPanel panel = new JPanel();
 		// 4. Add the header component to the quizPanel
-
+		createHeader("Dinosaurs");
+		quizPanel.add("Dinosaurs", null);
 		// 5. Add the quizPanel to the frame
-
+		frame.add(quizPanel);
 		// 6. Use the createButton method to set the value of firstButton
-
+		
 		// 7. Add the firstButton to the quizPanel
 
 		// 8. Write the code to complete the createButton() method below. Check that your
@@ -87,11 +88,11 @@ public class Jeopardy implements ActionListener {
 	private JButton createButton(String dollarAmount) {
 		
 		// Create a new JButton
-
+		JButton button = new JButton();
 		// Set the text of the button to the dollarAmount
-
+		button.setText("$100");
 		// Increment the buttonCount (this should make the layout vertical)
-
+		
 		// Return your new button instead of the temporary button
 
 		return new JButton("temporary button");
--- src/SimonSays.java
+++ src/SimonSays.java
@@ -35,6 +35,7 @@ public class SimonSays extends KeyAdapter {
 		images.put(new Integer(KeyEvent.VK_UP), "down.jpg");
 		images.put(new Integer(KeyEvent.VK_UP), "right.jpg");
 		images.put(new Integer(KeyEvent.VK_UP), "left.jpg");
+		
 		// 3. Use a JOptionPane to tell the user the rules: "Press the matching key when
 		// 'Simon says' otherwise press a different key"
 		JOptionPane.showMessageDialog(frame, "Press the matching key when 'Simon Says.' Otherwise press a different key.");
@@ -44,30 +45,40 @@ public class SimonSays extends KeyAdapter {
 
 	public void keyPressed(KeyEvent e) {
 		// 15. Make a points variable to track the score.
-
+			int points = 0;
 		// 16. If the keyCode matches the imageIndex and "Simon says"
-		
+			if (e.getKeyCode() == imageIndex && simonSays == true) {
+				points = points + 1;
+				speak("You are correct!");
+			}
 			// 17. Increase the value of score
 		
 			// 18. Use the speak method to tell the user they were correct
 		
 		// 19. If the keyCode doesn't match the imageIndex and "Simon didn't say..."
-		
+			else if (e.getKeyCode() != imageIndex && simonSays == false){
+				points = points + 1;
+				speak("You are correct!");
+				}
 			// 20.  Increase the value of score
 		
 			// 21. Use the speak method to tell the user they were correct
 		
 		// 22. Increment tries by 1
-		
+			tries = tries + 1;
 		// 25. If tries is greater than 9 (or however many you want)...
-		
+			if (tries > 9) {
+				JOptionPane.showMessageDialog(frame, "Your score is " + points);
+				
+			}
 			// 26. Tell the user their score
 		
 			// 27. Exit the program
 
 		// 23. Dispose of the frame
-
+			frame.dispose();
 		// 24. Call the showImage method to show a new image
+			showImage();
 	}
 
 	private void showImage() {
@@ -76,8 +87,7 @@ public class SimonSays extends KeyAdapter {
 		// 6. Set the frame to visible
 		frameb.setVisible(true);
 		// 7. Uncomment the following line to add a random image to your frame
-		frame.add(getNextRandomImage());
-
+		frameb.add(getNextRandomImage());
 		// 8. Set the name of your frame
 		frameb.setTitle("Simon Says");
 		// 9. Pack the frame
@@ -90,7 +100,8 @@ public class SimonSays extends KeyAdapter {
 		Random random = new Random();
 		// 13. Use the Random and the speak method to either say 
 		// "Simon says press this key" or "Press this key"
-		
+		speak("Simon says");
+		simonSays = true;
 		// 14. Above, set the value of simonSays to true/false appropriately
 		
 		
@@ -131,3 +142,8 @@ public class SimonSays extends KeyAdapter {
  * System.out.println((timeAtEnd.getTime()-timeAtStart.getTime())/1000);
  * System.exit(0);
  */
+
+
+
+
+
